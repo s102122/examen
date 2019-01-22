a23b8c9e12aeb0f2436f5d51ee8cdab8fad79334
s102122 <s102122@ap.be>
2019-01-22 15:57:44 +0100

    commit 3 express
    commit 3 express

--- express-inhaal/server.js
+++ express-inhaal/server.js
@@ -11,7 +11,7 @@ var db
 
 MongoClient.connect('mongodb://127.0.0.1:27017', (err, client) => {
     if(err) return console.log(err)
-    db = client.db('inhaal')
+    db = client.db('examen')
 
     app.listen(3000, function() {
         console.log('listening on 3000')
@@ -19,18 +19,30 @@ MongoClient.connect('mongodb://127.0.0.1:27017', (err, client) => {
 })
 
 app.get('/', (request, response) => {
-    db.collection('examen').find().toArray((err, result) => {
-        if(err) return console.log(err)
-
-        response.render('add.ejs', {quotes: result})
+    db.collection('inhaal').find().toArray((err, result) => {
+        if(err) return console.log(err)			
+		
+		response.render('add.ejs', {inhaal: result})
     })
 })
 
 app.post('/add', (request, response) => {
-    db.collection('examen').save(request.body, (err, result) => {
-        if(err) return console.log(err)
+	
+	var save = false
+	
+	if(db.collection('inhaal').find( { naam: request.body.naam, reden: request.body.reden, tijd: request.body.examen } ).toArray().length == 0) {
+	}				
+		db.collection('inhaal').save(request.body, (err, result) => {
+		if(err) return console.log(err)
 
-        console.log('saved to database')
-        response.redirect('/')
-    })
+			console.log('saved to database')
+			response.redirect('/')			
+	})	
+			
+})
+
+app.get('/search', (request, response) => {
+	db.collection('inhaal').find( { naam: request.body.naam }).sort({name:1}).toArray((err, result) => {
+		response.render('search.ejs', {inhaal: result})
+	})
 })
